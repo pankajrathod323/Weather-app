d7b99a61e74fa50b13f366b2d85494ae89bf2589
pankaj rathod <pankajrathod272004@gmail.com>
2026-01-06 15:49:41 +0530

    fixes issue in SearchBox.jsx

--- react-mini-project/src/SearchBox.jsx
+++ react-mini-project/src/SearchBox.jsx
@@ -11,7 +11,7 @@ export function SearchBox({updateInfo}){
   const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 
-  let getWeatherdata = async () => {
+  let getWeatherdata = async (city) => {
     let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
     let jsonResponse = await response.json();
     if(jsonResponse.cod === "404"){
@@ -37,9 +37,9 @@ export function SearchBox({updateInfo}){
   let handleSubmit = async (event) => {
     try{
       event.preventDefault();
-      setCity("");
-     let newInfo = await getWeatherdata();
+     let newInfo = await getWeatherdata(city);
      updateInfo(newInfo);
+     setCity("");
      setError(false);
     }
     catch(err){
